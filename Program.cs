503932d6053315764578c168839f0badb6da09d1
jgrissom <jgrissom91@gmail.com>
2024-10-01 19:43:33 -0500

    whitespace

--- Program.cs
+++ Program.cs
@@ -10,97 +10,100 @@ string file = "dk.csv";
 // make sure movie file exists
 if (!File.Exists(file))
 {
-    logger.Error("File does not exist: {File}", file);
+  logger.Error("File does not exist: {File}", file);
 }
 else
 {
-    // create list of characters
-    // lists are used since we do not know number of lines of data
-    // to populate the list with data, read from the data file
-    List<Character> characters = [];
-    try
+  // create list of characters
+  // lists are used since we do not know number of lines of data
+  // to populate the list with data, read from the data file
+  List<Character> characters = [];
+  try
+  {
+    StreamReader sr = new(file);
+    // first line contains column headers
+    sr.ReadLine();
+    while (!sr.EndOfStream)
     {
-        StreamReader sr = new(file);
-        // first line contains column headers
-        sr.ReadLine();
-        while (!sr.EndOfStream)
-        {
-            string? line = sr.ReadLine();
-            if (line is not null)
-            {
-                Character character = new();
-                // character details are separated with comma(,)
-                string[] characterDetails = line.Split(',');
-                // 1st array element contains id
-                character.Id = UInt64.Parse(characterDetails[0]);
-                // 2nd array element contains character name
-                character.Name = characterDetails[1] ?? string.Empty;
-                // 3rd array element contains character description
-                character.Description = characterDetails[2] ?? string.Empty;
-                characters.Add(character);
-            }
-        }
-        sr.Close();
+      string? line = sr.ReadLine();
+      if (line is not null)
+      {
+        Character character = new();
+        // character details are separated with comma(,)
+        string[] characterDetails = line.Split(',');
+        // 1st array element contains id
+        character.Id = UInt64.Parse(characterDetails[0]);
+        // 2nd array element contains character name
+        character.Name = characterDetails[1] ?? string.Empty;
+        // 3rd array element contains character description
+        character.Description = characterDetails[2] ?? string.Empty;
+        characters.Add(character);
+      }
     }
-    catch (Exception ex)
+    sr.Close();
+  }
+  catch (Exception ex)
+  {
+    logger.Error(ex.Message);
+  }
+  string? choice;
+  do
+  {
+    // display choices to user
+    Console.WriteLine("1) Add Character");
+    Console.WriteLine("2) Display All Characters");
+    Console.WriteLine("Enter to quit");
+
+    // input selection
+    choice = Console.ReadLine();
+    logger.Info("User choice: {Choice}", choice);
+
+    if (choice == "1")
     {
-        logger.Error(ex.Message);
+      // Add Character
+      Character character = new();
+      Console.WriteLine("Enter new character name: ");
+      character.Name = Console.ReadLine() ?? string.Empty;
+      if (!string.IsNullOrEmpty(character.Name))
+      {
+        // check for duplicate name
+        List<string> LowerCaseNames = characters.ConvertAll(character => character.Name.ToLower());
+        if (LowerCaseNames.Contains(character.Name.ToLower()))
+        {
+          logger.Info($"Duplicate name {character.Name}");
+        }
+        else
+        {
+          // generate id - use max value in Ids + 1
+          character.Id = characters.Max(character => character.Id) + 1;
+          // input character description
+          Console.WriteLine("Enter description:");
+          character.Description = Console.ReadLine() ?? string.Empty;
+          // create file from data
+          StreamWriter sw = new(file, true);
+          sw.WriteLine($"{character.Id},{character.Name},{character.Description}");
+          sw.Close();
+          // add new character details to Lists
+          characters.Add(character);
+          // log transaction
+          logger.Info($"Character id {character.Id} added");
+        }
+      }
+      else
+      {
+        logger.Error("You must enter a name");
+      }
     }
-    string? choice;
-    do
+    else if (choice == "2")
     {
-        // display choices to user
-        Console.WriteLine("1) Add Character");
-        Console.WriteLine("2) Display All Characters");
-        Console.WriteLine("Enter to quit");
-
-        // input selection
-        choice = Console.ReadLine();
-        logger.Info("User choice: {Choice}", choice);
-
-        if (choice == "1")
-        {
-            // Add Character
-            Character character = new();
-            Console.WriteLine("Enter new character name: ");
-            character.Name = Console.ReadLine() ?? string.Empty;
-            if (!string.IsNullOrEmpty(character.Name)){
-                // check for duplicate name
-                List<string> LowerCaseNames = characters.ConvertAll(character => character.Name.ToLower());
-                if (LowerCaseNames.Contains(character.Name.ToLower()))
-                {
-                    logger.Info($"Duplicate name {character.Name}");
-                }
-                else
-                {
-                    // generate id - use max value in Ids + 1
-                    character.Id = characters.Max(character => character.Id) + 1;
-                    // input character description
-                    Console.WriteLine("Enter description:");
-                    character.Description = Console.ReadLine() ?? string.Empty;
-                    // create file from data
-                    StreamWriter sw = new(file, true);
-                    sw.WriteLine($"{character.Id},{character.Name},{character.Description}");
-                    sw.Close();
-                    // add new character details to Lists
-                    characters.Add(character);
-                    // log transaction
-                    logger.Info($"Character id {character.Id} added");
-                }
-            } else {
-                logger.Error("You must enter a name");
-            }
-        }
-        else if (choice == "2")
-        {
-            // Display All Characters
-            // loop thru List
-            foreach(Character character in characters)
-            {
-                Console.WriteLine(character.Display());
-            }
-        }
-    } while (choice == "1" || choice == "2");
+      // Display All Characters
+      // loop thru List
+      foreach (Character character in characters)
+      {
+        Console.WriteLine(character.Display());
+      }
+    }
+  } while (choice == "1" || choice == "2");
 }
 
 logger.Info("Program ended");
